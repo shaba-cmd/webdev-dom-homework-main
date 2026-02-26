38bab62e64350aef54643027eb6faa1347ce3c7f
1DOGI <global.real@mail.ru>
2026-02-26 19:30:45 +0700

    Внедрил API

--- modules/methods.js
+++ modules/methods.js
@@ -1,3 +1,19 @@
 export const replaceMethod = (el) => {
-    return el.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+  return el.value.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+};
+
+export const nowDateTime = (date) => {
+  const formateDate = "2-digit";
+
+  const nowDate = new Date(date)
+    .toLocaleString("ru-RU", {
+      day: formateDate,
+      month: formateDate,
+      year: formateDate,
+      hour: formateDate,
+      minute: formateDate,
+    })
+    .replace(/,/, "");
+
+  return nowDate;
 };
--- modules/rendering.js
+++ modules/rendering.js
@@ -1,14 +1,16 @@
 import { comments } from "./comments.js";
+import { commentsButtons, likeButtons } from "./initListeners.js";
+import { nowDateTime } from "./methods.js";
 
 export const commentRendering = () => {
   const listEl = document.querySelector(".comments");
 
   const render = comments
-    .map((comment, index) => {
-      return `<li class="comment" data-comm="${index}">
+    .map((comment) => {
+      return `<li class="comment" data-id="${comment.id}">
           <div class="comment-header">
-            <div>${comment.name}</div>
-            <div>${comment.data}</div>
+            <div>${comment.author.name}</div>
+            <div>${nowDateTime(comment.date)}</div>
           </div>
           <div class="comment-body">
             <div class="comment-text">
@@ -17,8 +19,8 @@ export const commentRendering = () => {
           </div>
           <div class="comment-footer">
             <div class="likes">
-              <span class="likes-counter" data-index="${index}">${comment.counter}</span>
-              <button class="like-button ${comment.like ? "-active-like" : ""}"></button>
+              <span class="likes-counter" data-index="${comment.id}">${comment.likes}</span>
+              <button class="like-button ${comment.isLiked ? "-active-like" : ""}"></button>
             </div>
           </div>
         </li>`;
@@ -26,4 +28,7 @@ export const commentRendering = () => {
     .join("");
 
   listEl.innerHTML = render;
+
+  commentsButtons();
+  likeButtons();
 };
